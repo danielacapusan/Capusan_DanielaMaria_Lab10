44ee4bb0ade20e46db013a54604ffc92538006a5
Daniela-Maria Capusan <capusandaniela76@yahoo.com>
2020-12-19 22:06:56 +0200

    Lab11

---
+++ Capusan_DanielaMaria_Lab10/ValidationBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace Capusan_DanielaMaria_Lab10
+{
+    class ValidationBehaviour: Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor = string.IsNullOrEmpty(args.NewTextValue) ? Color.Red : Color.Default;
+        }
+    }
+}
